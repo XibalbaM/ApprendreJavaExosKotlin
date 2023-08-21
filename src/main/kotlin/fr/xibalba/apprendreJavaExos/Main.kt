dedb44fbc9cc2eea0856181b32ac39728833c00a
XibalbaM <maelporret@outlook.fr>
2023-08-21 22:10:06 +0200

    Update

--- src/main/kotlin/fr/xibalba/apprendreJavaExos/Main.kt
+++ src/main/kotlin/fr/xibalba/apprendreJavaExos/Main.kt
@@ -1,37 +1,68 @@
 package fr.xibalba.apprendreJavaExos
 
-val emojiMapping = mapOf(
-    "smile" to "D83D.DE00",
-    "joy" to "D83D.DE02",
-    "pleading_face" to "D83E.DD7A",
-    "rocket" to "D83D.DE80",
-    "cat_face" to "D83D.DC31"
-)
+import java.nio.file.Files
+import java.nio.file.Path
+
+val emojiMapping: MutableMap<String, String> by lazy {
+    if (Files.exists(Path.of("emojiMapping.txt"))) {
+        Files.readAllLines(Path.of("emojiMapping.txt")).map { it.split(" ") }.associate { it[0] to it[1] }.toMutableMap()
+    } else {
+        mutableMapOf(
+            "smile" to "D83D.DE00",
+            "joy" to "D83D.DE02",
+            "pleading_face" to "D83E.DD7A",
+            "rocket" to "D83D.DE80",
+            "cat_face" to "D83D.DC31"
+        )
+    }
+}
 
 fun main() {
     print("Text to decode: ")
     val text = readln()
+    val notMappedEmojis = mutableListOf<String>()
     val stringBuilder = StringBuilder()
     var wasPreviousCharEmoji = false
+    var wasPreviousCharUnreconizedEmoji = false
     text.toCharArray().forEachIndexed { index, char ->
-        if (!wasPreviousCharEmoji && char.code > 53248 && text.length > index + 1 && text[index + 1].code > 53248) {
-            val emojiCode = char.code.toHexStringWithoutZerosAtStart() + "." + text[index + 1].code.toHexStringWithoutZerosAtStart()
+        if (!wasPreviousCharEmoji && !wasPreviousCharUnreconizedEmoji && char.code > 53248 && text.length > index + 1 && text[index + 1].code > 53248) {
+            val emojiCode = emojiCode(char, text[index + 1])
             val emojiName = emojiMapping.filterValues { it == emojiCode }.keys.firstOrNull()
             if (emojiName != null) {
                 stringBuilder.append(":$emojiName:")
                 wasPreviousCharEmoji = true
             } else {
                 stringBuilder.append(char)
+                notMappedEmojis.add(char.toString() + text[index + 1].toString())
+                wasPreviousCharUnreconizedEmoji = true
             }
         } else {
             if (wasPreviousCharEmoji) {
                 wasPreviousCharEmoji = false
             } else {
                 stringBuilder.append(char)
+                wasPreviousCharUnreconizedEmoji = false
             }
         }
     }
     println("Decoded text: $stringBuilder")
+    if (notMappedEmojis.isNotEmpty()) {
+        print("Do you want to map missing emojis? (y/N) ")
+        val answer = readln()
+        if (answer.equals("y", true)) {
+            notMappedEmojis.forEach {
+                print("Emoji $it: ")
+                val emojiName = readln()
+                if (emojiName.isNotBlank())
+                    emojiMapping[emojiName] = emojiCode(it[0], it[1])
+            }
+            Files.write(Path.of("emojiMapping.txt"), emojiMapping.map { "${it.key} ${it.value}" })
+        }
+    }
+}
+
+fun emojiCode(char1: Char, char2: Char): String {
+    return char1.code.toHexStringWithoutZerosAtStart() + "." + char2.code.toHexStringWithoutZerosAtStart()
 }
 
 fun Int.toHexStringWithoutZerosAtStart(): String {
