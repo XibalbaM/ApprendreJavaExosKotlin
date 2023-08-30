23f067f97781730969c2afe99c0d36b2daadb0f9
XibalbaM <maelporret@outlook.fr>
2023-08-30 21:18:27 +0200

    Update

--- src/main/kotlin/fr/xibalba/apprendreJavaExos/Main.kt
+++ src/main/kotlin/fr/xibalba/apprendreJavaExos/Main.kt
@@ -1,59 +1,44 @@
 package fr.xibalba.apprendreJavaExos
 
-class TitleTag(val level: Int, val content: String)
+import kotlin.io.path.*
 
+@OptIn(ExperimentalPathApi::class)
 fun main() {
-    println("Welcome to HTML to summary converter !")
-    println("Choose your mode : (default 1)")
-    println("1 - Enter HTML code in console")
-    println("2 - Enter HTML code in file")
-    println("3 - Enter URL")
-    val modeLine = readln()
-    val mode = modeLine.matches(Regex("[123]")).let { if (it) modeLine.toInt() else 1 }
-    when (mode) {
-        1 -> {
-            println("Enter HTML code then \"end\" to validate your input :")
-            var html = ""
-            var line = readln()
-            while (line != "end") {
-                html += line
-                line = readln()
-            }
-            printTitlesFromHtml(html)
-        }
-
-        2 -> {
-            try {
-                println("Enter file path :")
-                val path = readln()
-                val html = java.io.File(path).readText()
-                printTitlesFromHtml(html)
-            } catch (e: Exception) {
-                println("An error occurred while processing the file.")
-            }
-        }
-
-        3 -> {
-            try {
-                println("Enter URL :")
-                val url = readln()
-                val html = java.net.URL(url).readText()
-                println(html)
-                printTitlesFromHtml(html)
-            } catch (e: Exception) {
-                println("An error occurred while processing the URL.")
-            }
+    println("Welcome to HTML tag stats !")
+    print("Please enter project path : ")
+    val path = Path(readln())
+    if (path.exists()) {
+        if (path.isDirectory()) {
+            val htmlFiles = path.walk().filter { it.toString().endsWith(".html") }.map { it.toFile().readText() }.toList().toTypedArray()
+            printStats(htmlFiles)
+        } else {
+            val html = path.readText()
+            printStats(arrayOf(html))
         }
     }
 }
 
-fun printTitlesFromHtml(html: String) {
-    val titles = extractTitlesFromHtml(html)
-    titles.forEach { println("---".repeat(it.level - 1) + it.content) }
+fun printStats(html: Array<String>) {
+    val tags = mutableMapOf<String, Int>()
+    for (file in html) {
+        val fileTags = extractTagsFromHtml(file)
+        for (tag in fileTags) {
+            if (tags.containsKey(tag.key)) {
+                tags[tag.key] = tags[tag.key]!! + tag.value
+            } else {
+                tags[tag.key] = tag.value
+            }
+        }
+    }
+    println("Tag\t\t\t\t\tOccurrences")
+    println("============================================")
+    for (tag in tags) {
+        println("${tag.key}\t\t\t\t\t${tag.value}")
+    }
 }
 
-fun extractTitlesFromHtml(html: String): Sequence<TitleTag> {
-    val regex = "<h([1-6])[^>]*>(.+?)</h\\1>".toRegex()
+fun extractTagsFromHtml(html: String): Map<String, Int> {
+    val regex = "<([a-z0-9-]+)[^>]*>.*</\\1>".toRegex()
     val matches = regex.findAll(html.replace("\n", ""))
-    return matches.map { TitleTag(it.groupValues[1].toInt(), it.groupValues[2]) }
+    return matches.map { it.groupValues[1] }.groupingBy { it }.eachCount()
 }
